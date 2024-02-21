40409a6d9d065750909c2951d76f0132ed0929fb
binz1215 <qnd1215@naver.com>
2024-02-22 01:41:44 +0900

    방향키 입력

--- source/source.c
+++ source/source.c
@@ -15,8 +15,34 @@
 
 int main() //
 {
-
-	ft_kbhit();
-
+	char pressKey;
+	//키가 입력되면 잡아준다.
+    init_keyboard();
+    while(true) {
+        // printf("looping\n");
+        // sleep(1);
+        if(kbhit()) {
+            pressKey = readch();//getch()대신
+			// printf("%c\n", pressKey);
+			switch (pressKey)
+			{
+				case 'w':
+					printf("화살표 위쪽 키 \n");
+					break;
+				case 's':
+					printf("화살표 아래쪽 키 \n");
+					break;
+				case 'a':
+					printf("화살표 왼쪽 키 \n");
+					break;
+				case 'd':
+					printf("화살표 오른쪽 키 \n");
+					break;
+			}
+        }
+    }
+    close_keyboard();
     return (0);
 }
+
+//여기까지 1강 내용
